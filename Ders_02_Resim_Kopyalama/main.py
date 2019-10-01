f8eb15169967b4aeb399e53d1bd2919e1ad447c4
as <asd>
2019-10-01 12:10:34 +0300

    Update
    Update

--- Ders_02_Resim_Kopyalama/main.py
+++
@@ -1,9 +0,0 @@
-import cv2
-
-
-resim_1 = cv2.imread('../Resimler/the_100.jpg')
-resim_2 = cv2.imwrite('../Resimler/yeni_resim.png', resim_1)
-
-cv2.imshow('The 100 Resmi', resim_1)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
